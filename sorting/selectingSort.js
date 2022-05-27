6973509eea1df72c612eeaa8cca5b2f5d7edb044
EmanSaeed331 <36940678+EmanSaeed331@users.noreply.github.com>
2022-05-27 16:56:11 +0200

    Add:SelectionSort

---
+++ sorting/selectingSort.js
@@ -0,0 +1,24 @@
+/**
+ * @complexity (O(N^2))
+ */
+let SelectingSort = (arr) =>{
+    let min_index = 0 ;
+    for(let i=0; i<arr.length; i++)
+        {
+            min_index = i ;
+            for(j=i+1 ; j < arr.length ; j++) {
+                if(arr[j] <arr[min_index]){
+                    min_index = j;
+
+                    swap(arr,min_index,i);
+                }
+            }
+        }
+        return arr ; 
+}
+let swap = (arr , xp , yp) =>{
+    let temp = arr[xp];
+    arr[xp] = arr[yp];
+    arr[yp] = temp;
+}
+console.log(SelectingSort([2,5,1,7,0]))
