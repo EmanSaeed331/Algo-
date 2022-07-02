a9dcb6e8d80709f9cb1c7f93ec324345f4041512
EmanSaeed331 <36940678+EmanSaeed331@users.noreply.github.com>
2022-07-02 23:36:38 +0200

    update

--- Searching/LinearSearch.js
+++ Searching/LinearSearch.js
@@ -3,7 +3,7 @@
  * @Complexity O(N)
  */
 
-let LinearSearch = (arr , target  ) =>{
+let LinearSearch =  (arr , target  ) =>{
     for (let i =0 ; i < arr.length ; i++){
         if (arr[i] == target){
             return i 
